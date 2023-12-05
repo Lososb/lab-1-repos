338dfec4c2beb9311ccfc00249028ec568234371
Lososb <58726460+Lososb@users.noreply.github.com>
2023-12-05 13:52:04 +0300

    first commit

---
+++ my first project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+class Programm
+{
+    static void main(String[] args)
+    {
+        Console.WriteLine("This is my first project!");
+    }
+}
+
